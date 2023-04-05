8e88f52b137cae6992b2bc80dd24a9c8b6f90ef2
Nicolas Guerrero <kriegern@users.noreply.github.com>
2023-04-05 17:42:45 -0400

    Mejorado

---
+++ nico.py
@@ -0,0 +1,13 @@
+nombre = 'Nicolás'
+edad = 19
+print('Hola soy '+nombre+' y tengo '+str(edad)+' años')
+#No se puede concadenar diferentes tipos de datos, por lo que se transforma el dato de edad a string.
+
+nombre='Andrés'
+#Se asigna nuevamente la variable nombre pero también se puede crear otra con nombre1 si es necesario tener ambas.
+print('Hola mi nuevo nombre es:',nombre)
+
+#Se usa input para pedir datos
+nombre = input('¿Cual es tu nombre?: \n')
+print('Su nombre es:',nombre)
+#El \n sirve para bajar una linea.
