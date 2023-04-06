5631473f0504cdfbce3a39a6e14c6adf4b79e2d1
Nicolas Guerrero <kriegern@users.noreply.github.com>
2023-04-06 15:57:47 -0400

    Clase 06/04/2023

---
+++ 02-clase6abril.py
@@ -0,0 +1,29 @@
+#Datos de un tipo numerico
+estatura = 1.71
+peso = 70
+complejo = 1 + 4j
+print("Impresión del número complejo",complejo)
+
+#Operacion aritmetica basica
+imc = peso/estatura**2
+print("Mi IMC es de:",imc)
+
+#Datos de tipo cadena de caracteres
+asignatura= 'programación'
+carrera= 'Ingenieria Civil en Informatica'
+print('La asignatura de',asignatura,'corresponde a la carrera de',carrera)
+
+#Valores booleanos
+ampolleta=False
+interruptor=True
+
+#Con type sabemos el tipo de datos que estamos tratando
+print(type(ampolleta))
+
+ampolleta='soy una ampolleta'
+print(type(ampolleta))
+#Datos tipo array (objetos de tipo colección)
+estudiantes = ['Matias', 'Marco', 'Cristobal', 'Sebastián']
+num=(1,2,3,4,5,6)
+print(estudiantes)
+print(num)
