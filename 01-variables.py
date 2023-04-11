d012bf4811349d4939425643316a9fafe958c2e1
Nicolas Guerrero <kriegern@users.noreply.github.com>
2023-04-11 15:11:57 -0400

    clase 11-04

---
+++ 01-variables.py
@@ -0,0 +1,37 @@
+#Declarando variables de tipo texto
+name='Nico'
+
+nombre = 'Nicolás'
+
+print('Hola soy',name)
+
+#Declarando variable de tipo númerico
+edad = 19
+
+print('Mi edad es',edad)
+
+print('Hola soy '+nombre+' y tengo '+str(edad)+' años')
+
+#No se puede concadenar diferentes tipos de datos con +, por lo que se transforma el dato de edad a string.
+#Es recomendable utilizar el signo + para unirlos, y a diferencia de la coma , no agrega espacios por lo que hay que añadirlos
+
+print(f"Hola mi nombre es {nombre} y tengo {edad} años")
+#Desde las ultimas versiones de python se puede usar los corchetes {} para poner las variables y asi hacerlo más amigable.
+nombre='Andrés'
+
+#Se asigna nuevamente la variable nombre pero también se puede crear otra con nombre1 si es necesario tener ambas.
+
+print('Hola mi nuevo nombre es:',nombre)
+
+#Se usa input para pedir datos
+
+nombre1 = input('¿Cual es tu nombre?: \n')
+
+print('Su nombre es:',nombre1)
+
+#El \n sirve para bajar una linea.
+#El identificador de una variable es su nombre.
+
+ciudad, region, pais, year = "Puerto Montt", "Los Lagos", "Chile", 2023
+#Se puede asignar varias variables a la vez, separandolas por comas.
+print(f"Hola vivo en {ciudad}, en la región de {region}, en el país de {pais}, y es el año {year}")
---
+++ 02-tipos-de-datos.py
@@ -0,0 +1,53 @@
+# Datos de un tipo numerico
+estatura = 1.71 #Real
+edad= 29 #Entero
+peso = 70.5 #Real
+complejo = 1 + 4j #Complejo, se pone j para los complejos
+print("Impresión del número complejo",complejo)
+print(f"Mi estatura es de {estatura}")
+# Operacion aritmetica basica para sacar el imc
+imc = peso/estatura**2
+print("Mi IMC es de:",imc)
+
+#El codigo {:.2f} es para aproximar y va entre comillas y .format es para 
+print("Mi IMC es de: {:.2f}" .format(imc), "\n")
+
+# Datos de tipo cadena de caracteres
+asignatura= 'programación'
+carrera= 'Ingenieria Civil en Informatica'
+print('La asignatura de',asignatura,'corresponde a la carrera de',carrera)
+print('La asignatura de',len(asignatura),'corresponde a la carrera de',len(carrera))
+#Funciones propias de python 
+#str() , int() , float() len() , type()= String es para texto, Int para enteros, float para reales, 
+#y len cuenta los caracteres, por ej: "Hola"= 4
+
+# Valores booleanos
+ampolleta=False
+interruptor=True
+
+# Con type sabemos el tipo de datos que estamos tratando
+print(type(ampolleta))
+
+ampolleta='soy una ampolleta'
+print(type(ampolleta))
+
+estudiantes = ['Matias', 'Marco', 'Cristobal', 'Sebastián']
+num=[1,2,3,4,5,6]
+lenguaje = ["Python"]
+data= ["Osorno", {"UV": "nivel bajo", "Temp ºC": 15}, (-40.5725, -70.432)]
+print(data)
+print(len(data))
+print("lista de cadena de caracteres:",estudiantes)
+print("lista de números:",num)
+print("lista de elementos:",lenguaje)
+#Con .count() podemos buscar un elemento dentro de una lista y nos muestra donde está.
+print(estudiantes.count("Matias"))
+#Datos tipo array (objetos de tipo colección) o arreglos. Con array se gasta menos memoria que con list() pero list() es mas dinamico
+#y array es mas limitado a un tipo de dato.
+nueva_lista = list()
+print("Esta es una lista vacia",nueva_lista)
+# los corchetes [] se utilizan para crear una lista
+# que es una colección ordenada, los elementos de la lista están separados por comas
+
+
+
