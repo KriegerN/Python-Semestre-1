54e52522c7f9e0916942980a9ba8db9079b876f9
Nicolas Guerrero <kriegern@users.noreply.github.com>
2023-05-03 17:57:16 -0400

    03-05

---
+++ Clases/05-bucles.py
@@ -0,0 +1,20 @@
+
+'01 - CICLOS WHILE'
+num=0
+while num<= 100:
+    print(num)
+    num+= 2
+print("Primer bucle terminado")
+
+'BUCLE INFINITO'
+edad=19
+#while edad>=18: 
+#    print("Eres mayor de edad")
+
+'Combinando while y else'
+while num<= 200:
+    print(num)
+    num+= 2
+else:
+    print("Mi condicion es igual o mayor a 200")
+print("Segundo bucle terminado")
