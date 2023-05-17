89d01a988f4611d015564c0d6e6916f7840392f6
Nicolas Guerrero <KriegerN@users.noreply.github.com>
2023-05-17 00:04:24 -0400

    17-05

--- Guias/ejercicio10.py
+++ Guias/ejercicio10.py
@@ -11,7 +11,8 @@ agenda['Redes sociales']={
     'Twitter':'nico_twitter'
     }
 print(f'El facebook de {agenda["Nombre"]} es {agenda["Redes sociales"]["Facebook"]}')
-print(f'''La agenda completa actualizada es: Nombre: {agenda["Nombre"]}
+print(f'''La agenda completa actualizada es: 
+Nombre: {agenda["Nombre"]}
 Dirección: {agenda["Dirección"]}
 Ciudad: {agenda["Ciudad"]}
 Número telefónico: {agenda["Número telefónico"]}
