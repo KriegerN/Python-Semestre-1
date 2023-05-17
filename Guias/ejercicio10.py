5646f456fd7753c4fbca19595bb0b38b378b26db
Nicolas Guerrero <KriegerN@users.noreply.github.com>
2023-05-17 00:01:55 -0400

    .

--- Guias/ejercicio10.py
+++ Guias/ejercicio10.py
@@ -4,10 +4,19 @@ agenda={
     'Ciudad':'Osorno',
     'Número telefónico':'923874629'
     }
+
 agenda['Redes sociales']={
     'Facebook':'nico_facebook',
     'Instagram':'nico_instagram',
     'Twitter':'nico_twitter'
     }
-print(f'El facebook del contacto es {agenda["Redes sociales"]["Facebook"]}')
-print(f'La agenda completa actualizada es: {agenda} \n')
+print(f'El facebook de {agenda["Nombre"]} es {agenda["Redes sociales"]["Facebook"]}')
+print(f'''La agenda completa actualizada es: Nombre: {agenda["Nombre"]}
+Dirección: {agenda["Dirección"]}
+Ciudad: {agenda["Ciudad"]}
+Número telefónico: {agenda["Número telefónico"]}
+Redes sociales: 
+    Facebook: {agenda["Redes sociales"]['Facebook']}
+    Instagram: {agenda["Redes sociales"]['Instagram']}
+    Twitter: {agenda["Redes sociales"]['Twitter']}
+''')
