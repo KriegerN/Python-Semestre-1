99c900ba5ad1dde700763b532e10ed25957fb7e8
Nicolas Guerrero <KriegerN@users.noreply.github.com>
2023-05-16 21:30:24 -0400

    .

--- Guias/ejercicio01.py
+++ Guias/ejercicio01.py
@@ -2,5 +2,8 @@ n1=int(input("Ingrese el primer número: "))
 n2=int(input("Ingrese el segundo número: "))
 n3=int(input("Ingrese el tercer número: "))
 numeros=[n1,n2,n3]
-print(f"El número más grande es {max(numeros)}")
-print(f"El número más pequeño es {min(numeros)}")
+if n1==n2 and n2==n3:
+    print('Los tres números son iguales')
+else:
+    print(f"El número más grande es {max(numeros)}")
+    print(f"El número más pequeño es {min(numeros)}")
--- Guias/ejercicio09.py
+++ Guias/ejercicio09.py
@@ -1,7 +1,7 @@
 numeros=[4,3,8,12,6,10,14,3,6]
 print(f'La lista es: {numeros} \n')
 
-del numeros[-1]
+numeros.pop(-1)
 print(f'La lista con el ultimo numero eliminado es: {numeros} \n')
 
 numeros.insert(0,2)
@@ -11,10 +11,17 @@ numeros=list(set(numeros))
 print(f'Eliminando los duplicados {numeros} \n')
 
 numeros=[4,3,8,12,6,10,14,3,6]
+# ----  Media  ----
 cantidad=len(numeros)
 suma=sum(numeros)
 media=round((suma/cantidad),1)
-numeros_ordenados=sorted(numeros)
-mediana_index=round((cantidad/2)-1)
-mediana=numeros_ordenados[mediana_index]
-print(f'La lista ordenada es {numeros_ordenados}, su media es de {media} y su mediana es {mediana} \n')
+# ---- Mediana ----
+numeros.sort()
+mediana_index=(cantidad/2)-1
+if (mediana_index)%1==0.5:
+    mediana1=numeros[round(mediana_index)]
+    mediana2=numeros[round(mediana_index)+1]
+    mediana=(mediana1+mediana2)/2
+else:
+    mediana=numeros[mediana_index]
+print(f'La lista ordenada es {numeros} y su media es de {media} y su mediana es {mediana}')
