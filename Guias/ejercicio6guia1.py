62e24973f36b4a3079e3678e0dcfda930495a1c0
Nicolas Guerrero <kriegern@users.noreply.github.com>
2023-05-25 16:28:44 -0400

    .

--- Guias/ejercicio6guia1.py
+++ Guias/ejercicio6guia1.py
@@ -6,22 +6,22 @@ while hour!=23 or min!=59 or seg!=59:
         min+=1
     elif min==60:
         min=0
-        hour+=1
+        hour+=1 
     else:
         seg+=1
-    if hour in range (0,10) and min in range(0,10) and seg in range(0,10):
+    if hour in range(0,10) and min in range(0,10) and seg in range(0,10):
         print(f'La hora es: 0{hour}:0{min}:0{seg}')
-    elif hour in range (0,10) and min in range(0,10) and seg not in range(0,10):
+    elif hour in range(0,10) and min in range(0,10) and seg not in range(0,10):
         print(f'La hora es: 0{hour}:0{min}:{seg}')
-    elif  hour in range (0,10) and min not in range(0,10) and seg not in range(0,10):
+    elif  hour in range(0,10) and min not in range(0,10) and seg not in range(0,10):
         print(f'La hora es: 0{hour}:{min}:{seg}')
-    elif  hour in range (0,10) and min not in range(0,10) and seg in range(0,10):
+    elif  hour in range(0,10) and min not in range(0,10) and seg in range(0,10):
         print(f'La hora es: 0{hour}:{min}:0{seg}')
-    elif  hour not in range (0,10) and min in range(0,10) and seg in range(0,10):
+    elif  hour not in range(0,10) and min in range(0,10) and seg in range(0,10):
         print(f'La hora es: {hour}:0{min}:0{seg}')
-    elif  hour not in range (0,10) and min in range(0,10) and seg not in range(0,10):
+    elif  hour not in range(0,10) and min in range(0,10) and seg not in range(0,10):
         print(f'La hora es: {hour}:0{min}:{seg}')
-    elif  hour not in range (0,10) and min not in range(0,10) and seg in range(0,10):
+    elif  hour not in range(0,10) and min not in range(0,10) and seg in range(0,10):
         print(f'La hora es: {hour}:{min}:0{seg}')
     else: 
         print(f'La hora es: {hour}:{min}:{seg}')
