4326c2fb3c5a215fa7d4e55e1b367e75a028ac3b
Nicolas Guerrero <kriegern@users.noreply.github.com>
2023-05-25 09:34:30 -0400

    25-05

--- Guias/ejercicio5guia1.py
+++ Guias/ejercicio5guia1.py
@@ -3,5 +3,5 @@ lista=[]
 for i in range(0,20):
     lista.append(random.randint(40,350))
     print(f'El número elegido al azar es: {lista[i]}')
-buscar=int(input('Ingrese un número para conocer su número de ocurrencias: '))
-print(lista.count(buscar))
+buscar=int(input('Ingrese un número para conocer su cantidad de ocurrencias: '))
+print(f'La cantidad de ocurrencias del número {buscar} es de: {lista.count(buscar)}')
