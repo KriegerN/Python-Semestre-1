99bea48aa93ae6d0ced99a111d35bc5d249bc240
Nicolas Guerrero <kriegern@users.noreply.github.com>
2023-06-01 14:01:22 -0400

    .

--- Parciales/Parcial 2/ejercicio1.py
+++ Parciales/Parcial 2/ejercicio1.py
@@ -1,10 +1,7 @@
 def palindromo():
     print('--- Determinar si una palabra ingresada por teclado es una palíndromo ---')
     palabra=str(input('Ingrese una palabra: ').lower())
-    lista=[]
-    for i in palabra:
-        lista.insert(0,i)
-    palabraalreves=''.join(lista)
+    palabraalreves=palabra[1:]
     print(palabraalreves)
     if palabra==palabraalreves:
         print(f'La palabra {palabra} es un palíndromo')
