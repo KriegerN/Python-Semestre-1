b66173751f06783a9d0dedcb99deedc340a17cd3
Nicolas Guerrero <kriegern@users.noreply.github.com>
2023-06-01 01:54:07 -0400

    .

--- Guias/ejercicio6guia1.py
+++ Guias/ejercicio6guia1.py
@@ -1,31 +1,33 @@
-print('---- Reloj digital desde las 00:00:00 hasta las 23:59:59 ----')
-seg,min,hour=0,0,0
-while hour!=23 or min!=59 or seg!=59:
-    if seg==60:
-        seg=0
-        min+=1
-    elif min==60:
-        min=0
-        hour+=1 
-    else:
-        seg+=1
-    if hour in range(0,10) and min in range(0,10) and seg in range(0,10):
-        print(f'La hora es: 0{hour}:0{min}:0{seg}')
-    elif hour in range(0,10) and min in range(0,10) and seg not in range(0,10):
-        print(f'La hora es: 0{hour}:0{min}:{seg}')
-    elif  hour in range(0,10) and min not in range(0,10) and seg not in range(0,10):
-        print(f'La hora es: 0{hour}:{min}:{seg}')
-    elif  hour in range(0,10) and min not in range(0,10) and seg in range(0,10):
-        print(f'La hora es: 0{hour}:{min}:0{seg}')
-    elif  hour not in range(0,10) and min in range(0,10) and seg in range(0,10):
-        print(f'La hora es: {hour}:0{min}:0{seg}')
-    elif  hour not in range(0,10) and min in range(0,10) and seg not in range(0,10):
-        print(f'La hora es: {hour}:0{min}:{seg}')
-    elif  hour not in range(0,10) and min not in range(0,10) and seg in range(0,10):
-        print(f'La hora es: {hour}:{min}:0{seg}')
-    else: 
-        print(f'La hora es: {hour}:{min}:{seg}')
-# for hora in range(24):
-#     for minuto in range(60):
-#         for segundo in range(60):
-#             print(f'{hora:02}:{minuto:02}:{segundo:02}')
+# print('---- Reloj digital desde las 00:00:00 hasta las 23:59:59 ----')
+# seg,min,hour=0,0,0
+# while hour!=23 or min!=59 or seg!=59:
+#     if seg==60:
+#         seg=0
+#         min+=1
+#     elif min==60:
+#         min=0
+#         hour+=1 
+#     else:
+#         seg+=1
+#     if hour in range(0,10) and min in range(0,10) and seg in range(0,10):
+#         print(f'La hora es: 0{hour}:0{min}:0{seg}')
+#     elif hour in range(0,10) and min in range(0,10) and seg not in range(0,10):
+#         print(f'La hora es: 0{hour}:0{min}:{seg}')
+#     elif  hour in range(0,10) and min not in range(0,10) and seg not in range(0,10):
+#         print(f'La hora es: 0{hour}:{min}:{seg}')
+#     elif  hour in range(0,10) and min not in range(0,10) and seg in range(0,10):
+#         print(f'La hora es: 0{hour}:{min}:0{seg}')
+#     elif  hour not in range(0,10) and min in range(0,10) and seg in range(0,10):
+#         print(f'La hora es: {hour}:0{min}:0{seg}')
+#     elif  hour not in range(0,10) and min in range(0,10) and seg not in range(0,10):
+#         print(f'La hora es: {hour}:0{min}:{seg}')
+#     elif  hour not in range(0,10) and min not in range(0,10) and seg in range(0,10):
+#         print(f'La hora es: {hour}:{min}:0{seg}')
+#     else: 
+#         print(f'La hora es: {hour}:{min}:{seg}')
+import time
+for hora in range(1,24):
+    for minuto in range(52,60):
+        for segundo in range(60):
+            print(f'{hora:02}:{minuto:02}:{segundo:02}')
+            time.sleep(1)
